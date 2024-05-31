1caeb3db5ed81c507b4f451ba09b38337f4629ff
Mayank Jha <109874709+Code-With-Mayank@users.noreply.github.com>
2024-05-31 23:10:29 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -505,12 +505,11 @@ def main():
     st.set_page_config(page_title="Smart-Sales", page_icon=":bar_chart:", layout="wide")
     st.title(" :bar_chart: :orange[-- * SMART-SALES * --] :bar_chart:")   
     st.subheader(":money_with_wings: :violet[Where Data Meets Destiny] :money_with_wings:")
-    image = Image.open("images/smart_sales_logo.jpeg")
+    image = Image.open("static/smart_sales_logo.jpeg")
     st.sidebar.image(image, caption='ANALYZE - VISUALIZE - GENERATE',use_column_width="always")    
     st.markdown('<style>div.block-container{padding-top:0 rem;}</style>',unsafe_allow_html=True)
     
 
-    
     #----- PATH SETINGS ----------------#
     current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
     css_file = current_dir / "main.css"
