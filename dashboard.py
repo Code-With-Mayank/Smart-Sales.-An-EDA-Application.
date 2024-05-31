f2b4946d17c9d4e1c75fca8b00211f6ba140f86a
Mayank Jha <109874709+Code-With-Mayank@users.noreply.github.com>
2024-05-31 17:09:32 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -23,7 +23,8 @@ import urllib.parse
 
 warnings.filterwarnings('ignore')
 
-#Pdfkit path configuration
+# Pdfkit path configuration
+# Update the path to use the local wkhtmltopdf executable
 path_to_wkhtmltopdf = os.path.abspath(os.path.join(os.path.dirname(__file__), 'bin', 'wkhtmltopdf'))
 config = pdfkit.configuration(wkhtmltopdf=path_to_wkhtmltopdf)
 
